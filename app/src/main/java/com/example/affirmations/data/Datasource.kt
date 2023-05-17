a600f1fcf54902460a3bf7467ae19a5a2dc3983a
Alitor004 <irmaosgomes1302@gmail.com>
2023-05-17 20:33:39 -0300

    Adição das imagens.

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -6,16 +6,16 @@ import com.example.affirmations.model.Affirmation
 class Datasource {
     fun loadAffirmations(): List<Affirmation>{
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation01),
-            Affirmation(R.string.affirmation02),
-            Affirmation(R.string.affirmation03),
-            Affirmation(R.string.affirmation04),
-            Affirmation(R.string.affirmation05),
-            Affirmation(R.string.affirmation06),
-            Affirmation(R.string.affirmation07),
-            Affirmation(R.string.affirmation08),
-            Affirmation(R.string.affirmation09),
-            Affirmation(R.string.affirmation10)
+            Affirmation(R.string.affirmation01, R.drawable.image1),
+            Affirmation(R.string.affirmation02, R.drawable.image2),
+            Affirmation(R.string.affirmation03, R.drawable.image3),
+            Affirmation(R.string.affirmation04, R.drawable.image4),
+            Affirmation(R.string.affirmation05, R.drawable.image5),
+            Affirmation(R.string.affirmation06, R.drawable.image6),
+            Affirmation(R.string.affirmation07, R.drawable.image7),
+            Affirmation(R.string.affirmation08, R.drawable.image8),
+            Affirmation(R.string.affirmation09, R.drawable.image9),
+            Affirmation(R.string.affirmation10, R.drawable.image10)
         )
     }
 }
